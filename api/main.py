54b1fba4e05e3feb26794ee7121dde3cc1a9f15b
anywaysoul92 <anywaysoul92@gmail.com>
2026-04-17 16:24:05 +0900

    worker practice with redis Pub/Sub

--- api/main.py
+++ api/main.py
@@ -1,14 +1,49 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from connection import SessionFactory
+import uuid # 중복 없이 id 만들어줌 
+import json
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
+from redis import asyncio as aredis
 
+
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True) # ("redis://<ip이름>:6379")
+# decode_responses=True : 문자열로 바꿔달라는 의미 
 
 app = FastAPI()
 
-@app.get("/health-check")
-def health_check_handler():
-    with SessionFactory() as session:
-        stmt = text("SELECT * FROM user LIMIT 1;")
-        row = session.execute(stmt).fetchone() 
-    return {"user": row._asdict()} # row._asdict(): fastapi(json)error 방지를 위해 dict형태로 변환
-    
+@app.post("/chats")
+async def generate_chat_handler(
+    # 1) 요청 본문: user_input 입력
+    user_input: str = Body(..., embed=True)
+):  
+    # 2) 안전하게 subscribe먼저, channel
+
+    channel = str(uuid.uuid4())
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
+
+    # 3) 처리 : queue를 통해 worker에 queue를 통해 job을 전달 (= enqueue 한다)
+    task = {"channel": channel, "user_input": user_input}
+    await redis_client.lpush("queue", json.dumps(task))
+
+    # 4) 채널 메세지 읽기(대기), 토큰 반환
+    async def event_generator():
+
+        async for message in pubsub.listen():
+            if message["type"] != "message":
+                # 실제 메세지가 아닌 상태메세지는 무시
+                continue
+
+            token = message["data"]
+            if token == "[DONE]":
+                break
+            yield token
+
+        await pubsub.unsubscribe(channel)
+        await pubsub.close()
+
+
+    # 5) 결과 수신
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream",
+    )
--- worker/main.py
+++ worker/main.py
@@ -1,5 +1,8 @@
+import redis
+import json
 from llama_cpp import Llama
 
+redis_client = redis.from_url("redis://redis:6379", decode_responses=True)
 
 llm = Llama(
     model_path="./models/Llama-3.2-1B-Instruct-Q4_K_M.gguf",
@@ -16,15 +19,36 @@ SYSTEM_PROMPT = (
     "Do not mix languages."
 )
 
-user_input = "python이 뭐야?"
 
-response = llm.create_chat_completion(
-    messages=[
-        {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": user_input},
-    ],
-    max_tokens=256,
-    temperature=0.7,
-)
+def run():
+    while True:
 
-print(response["choices"][0]["message"]["content"])
+        # 1) queue에서 task를 dequeue
+        _, task = redis_client.brpop("queue") #brpop, blockRPOP queue에 task 올 때까지 기다림 
+
+        task_data: dict = json.loads(task)
+
+        # 2) (반복) 추론 -> 토큰 -> publish
+        response_generator = llm.create_chat_completion(
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": task_data["user_input"]},
+            ],
+            max_tokens=256,
+            temperature=0.7,
+            stream=True
+        )
+        channel = task_data["channel"]   
+        for chunk in response_generator:
+            token = chunk["choices"][0]["delta"].get("content")
+            if token:
+                redis_client.publish(channel, token)
+
+
+        # 3) 추론 종료 알림: [Done] 전송
+        redis_client.publish(channel, "[DONE]")
+
+        #이 파일이 직접 실행한 경우에만, run() 호출 - 관례적 코드
+
+if __name__ == "__main__":
+    run()
